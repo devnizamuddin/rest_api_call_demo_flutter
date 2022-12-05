1d447fd164d3192980eb21cdf98641825da78339
nizamuddinshamrat <nshamrat7@gmail.com>
2022-12-06 03:51:22 +0600

    home view updated with routing to user and bus

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../routes/app_pages.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -9,14 +10,45 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: const Text('Home'),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: MenuItem('User', () {
+                      controller.onTapMenu(Routes.USER);
+                    }),
+                  ),
+                  Expanded(
+                    child: MenuItem('Bus', () {
+                      controller.onTapMenu(Routes.USER);
+                    }),
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+
+  Widget MenuItem(String name, VoidCallback ontap) {
+    return InkWell(
+      onTap: ontap,
+      child: SizedBox(
+        height: Get.width / 3,
+        child: Card(
+          child: Center(
+            child: Text(
+              name,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
         ),
       ),
     );

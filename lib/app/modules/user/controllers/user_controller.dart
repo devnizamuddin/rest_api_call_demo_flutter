a233c5831a7be47762bbc64af919dd5c4cb68751
nizamuddinshamrat <nshamrat7@gmail.com>
2022-12-06 06:05:53 +0600

    user list geting with pagination done

--- lib/app/modules/user/controllers/user_controller.dart
+++ lib/app/modules/user/controllers/user_controller.dart
@@ -1,23 +1,47 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:rest_api_call_demo_flutter/app/modules/user/models/user.dart';
+import 'package:rest_api_call_demo_flutter/app/services/api_communication.dart';
 
 class UserController extends GetxController {
-  //TODO: Implement UserController
+  late ApiCommunication _apiCommunication;
 
-  final count = 0.obs;
+  RxList<UserModel> userList = List<UserModel>.empty(growable: true).obs;
+  final Rx<int> _currentPageNumber = 1.obs;
+  final Rx<int> _totalPageCount = 0.obs;
+  late ScrollController scrollController;
+
+  getUsers() async {
+    final response =
+        await _apiCommunication.getData('users?page=$_currentPageNumber');
+    if (response != null) {
+      Map<String, dynamic> mapdata = response.data;
+      final list =
+          (mapdata['data'] as List).map((x) => UserModel.fromMap(x)).toList();
+      _totalPageCount.value = mapdata['total_pages'];
+      userList.addAll(list);
+      debugPrint(userList.length.toString());
+    }
+  }
+
   @override
-  void onInit() {
+  void onInit() async {
+    _apiCommunication = ApiCommunication();
+    getUsers();
+    scrollController = ScrollController();
+    scrollController.addListener(() {
+      if (_currentPageNumber.value != _totalPageCount.value) {
+        _currentPageNumber.value += 1;
+        getUsers();
+      }
+    });
     super.onInit();
   }
 
   @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
   void onClose() {
+    _apiCommunication.endConnection();
+    scrollController.dispose();
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/user/views/user_view.dart
+++ lib/app/modules/user/views/user_view.dart
@@ -3,22 +3,61 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/user_controller.dart';
+import '../models/user.dart';
 
 class UserView extends GetView<UserController> {
   const UserView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('UserView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'UserView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: const Text('User'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Obx(() {
+            return ListView.builder(
+              controller: controller.scrollController,
+              itemCount: controller.userList.length,
+              itemBuilder: (BuildContext context, int index) {
+                UserModel userModel = controller.userList[index];
+                return Card(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          const SizedBox(width: 10),
+                          Expanded(
+                            flex: 1,
+                            child: Image.network(
+                              userModel.avatar ?? '',
+                              height: 104,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Text(userModel.first_name ?? ''),
+                                  Text(userModel.last_name ?? ''),
+                                  Text(userModel.email ?? ''),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }),
+        ));
   }
 }
